1ee9605623f40b77c8dbe585c4056a29230041c6
patraRahul5678 <kanhapatra801@gmail.com>
2026-03-03 12:36:53 +0530

    new

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,14 @@ const express = require('express');
 const app = express();
 const webControllers = require('./controller/webhookController');
 const connectDB = require('./config/database');
+const port=process.env.PORT || 3000;
 
 app.use(express.json());
 
 
 app.post("/webhook", webControllers);
 
-app.listen(3000, async () => {
+app.listen(port, async () => {
     await connectDB();
-    console.log("Server is running on port 3000");
+    console.log(`Server is running on port ${port}`);
 });
