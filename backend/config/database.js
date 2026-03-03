e958f34c92b998282943bd266e25cc8a01af507f
patraRahul5678 <kanhapatra801@gmail.com>
2026-03-03 11:58:02 +0530

    new

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URI);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log("MongoDB Connected: DecisionGrid");
   } catch (error) {
     console.error(`Error: ${error.message}`);
     console.log('Make sure MongoDB is running on your machine');
